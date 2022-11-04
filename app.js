d758a306f06a9964c2d92ec4eda8c73b6eb92c9a
Everton Figueiredo <j.everton.figueiredo@gmail.com>
2022-11-04 18:25:23 -0300

    Teste de Vercel

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import * as dotenv from "dotenv";
 
-import { loginRouter, postRouter, commentaryRouter } from "./Routes/index.js";
+// import { loginRouter, postRouter, commentaryRouter } from "./Routes/index.js";
 
 dotenv.config();
 const app = express();
@@ -13,25 +13,25 @@ app.use(express.urlencoded({ extended: true }));
 
 // ------------ rotas principais ------------//
 
-app.post("/register", loginRouter);
-app.post("/login", loginRouter);
-app.get("/logado", loginRouter);
-app.get("/user", loginRouter);
+// app.post("/register", loginRouter);
+// app.post("/login", loginRouter);
+// app.get("/logado", loginRouter);
+// app.get("/user", loginRouter);
 
-//  --------------- postagens --------------- //
+// //  --------------- postagens --------------- //
 
-app.post("/user/:userId/post", postRouter);
-app.get("/post", postRouter);
-app.get("/user/:userId/post", postRouter);
-app.delete("/user/:userId/post/:postId", postRouter);
+// app.post("/user/:userId/post", postRouter);
+// app.get("/post", postRouter);
+// app.get("/user/:userId/post", postRouter);
+// app.delete("/user/:userId/post/:postId", postRouter);
 
-//  --------------- Comentary --------------- //
+// //  --------------- Comentary --------------- //
 
-app.post("/user/:userId/post/:postId/comment", commentaryRouter);
-app.get("/user/:userId/post/:postId/comment/:commentId", commentaryRouter);
-app.get("/comment", commentaryRouter);
-app.get("/commente", commentaryRouter);
-app.get("/test", commentaryRouter);
+// app.post("/user/:userId/post/:postId/comment", commentaryRouter);
+// app.get("/user/:userId/post/:postId/comment/:commentId", commentaryRouter);
+// app.get("/comment", commentaryRouter);
+// app.get("/commente", commentaryRouter);
+// app.get("/test", commentaryRouter);
 
 // ------------ rotas de teste ------------ //
 
@@ -41,7 +41,7 @@ app.get("/",function(req, res) {
     return res.json("Teste");
 });
 
-app.delete("/delete/:id", loginRouter);
+// app.delete("/delete/:id", loginRouter);
 
 // ------------ socket io ------------ //
 
